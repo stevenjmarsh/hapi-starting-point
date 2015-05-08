0aa55ef8b9fdf973a6fc862b6c3924391efcfe85
stevenjmarsh <sjm.atl@gmail.com>
2015-05-08 15:48:26 -0700

    refactor data css attr name

--- public/js/hbp_navbar.js
+++ public/js/hbp_navbar.js
@@ -1,7 +1,7 @@
 var hbpNavbar =  {
   setActiveMenuItems: function (menuItem) {
     // Script to show active page on navbar
-    $("#navbar-collapse-1 li[data-menuoption=" + menuItem + "]").addClass('active');
+    $("#navbar-collapse-1 li[data-menu-option=" + menuItem + "]").addClass('active');
 
     // If a dropdown sub menu page is shown, set the main dropdown menu item as active too
     //    NOTE: if have more than one dropdown menu, may need multiple if statments (and to select by id)
